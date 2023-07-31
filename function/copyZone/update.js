796359d78a280459067445feaee62a4249ccaf73
binhongtea <kuang2713ben@163.com>
2023-07-31 12:32:22 +0800

    new-function

--- function/copyZone/update.js
+++ function/copyZone/update.js
@@ -1,4 +1,4 @@
-var version = 20230729;
+var version = 230729;
 function update(){
     document.getElementById("updateDate").innerHTML =  "";
     document.getElementById("updateChange").innerHTML =  "";
@@ -10,18 +10,18 @@ function update(){
     xmlHttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         var VersionInfo = JSON.parse(this.responseText);
-        document.getElementById("updateDate").innerHTML =  "【提示】:<br>若显示 有可用的版本更新 ,则有新版本了<br>更新日期：" + VersionInfo.updateDate;
+        document.getElementById("updateDate").innerHTML =  "更新日期：" + VersionInfo.updateDate;
         document.getElementById("updateChange").innerHTML =  "新版变化：<br>" + VersionInfo.change;
         document.getElementById("updateNote").innerHTML =  VersionInfo.note;
         document.getElementById("updateSpinner").style = VersionInfo.spinner;
         if (VersionInfo.versionCode > version){
             document.getElementById("updateAvailable").style.display = "block";
         }
-        if (VersionInfo.versionCode == version){
+        if (VersionInfo.versionCode = version){
             document.getElementById("alreadyUpdate").style.display = "block";
         }
       }
     };
-    xmlHttp.open("GET", "https://raw.githubusercontent.com/binhongtea/kk-tool/main/version.json", true);
+    xmlHttp.open("GET","https://binhongtea.github.io/version.json",true);
     xmlHttp.send();
 }
